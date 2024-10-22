74a8fd911e5e90a49455d20270ab08b0d76974e3
meetrai007 <harmeetsingh77216@gmail.com>
2024-10-22 13:13:15 +0530

    modify model for upload product photos

--- e_commerse/store/models.py
+++ e_commerse/store/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     slug = AutoSlugField(populate_from='name', unique=True)
+    photo = models.ImageField(upload_to='products/', blank=True, null=True) 
 
     def __str__(self):
         return self.name
--- e_commerse/store/urls.py
+++ e_commerse/store/urls.py
@@ -1,8 +1,17 @@
 # urls.py
+
+# urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from .views import home_view, product_detail_view
+from .views import home_view,product_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('product/<slug:slug>/', product_detail_view, name='product_detail'),
+
 ]
+
+# Add this to serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
