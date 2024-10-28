1d08eb8b926d9663032a77dc848121cdcea0716d
meetrai007 <harmeetsingh77216@gmail.com>
2024-10-28 09:54:10 +0530

    add login and sign up page and make products tiles

---
+++ e_commerse/base/urls.py
@@ -0,0 +1,14 @@
+# urls.py
+
+# urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from base import views
+
+urlpatterns = [
+    path('login/',views.login, name='login'),
+    path('signup/',views.signup, name='signup'),
+
+]
+
--- e_commerse/e_commerse/urls.py
+++ e_commerse/e_commerse/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/',include('store.urls')),
+    path('base/',include('base.urls')),
 ]
