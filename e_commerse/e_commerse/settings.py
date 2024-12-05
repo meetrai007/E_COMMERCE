abc90429619ea8b756262683375450e404961bd1
meetrai007 <harmeetsingh77216@gmail.com>
2024-12-05 20:19:35 +0530

    improve footer css

--- e_commerse/e_commerse/settings.py
+++ e_commerse/e_commerse/settings.py
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'e_commerse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tutorial',
+        'USER': 'root',
+        'PASSWORD': 'Coder@007',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
