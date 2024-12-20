18458f9d6f8eded30bd9638f15a6c194479aab01
meetrai007 <meetrai101@gmail.com>
2024-12-20 16:40:45 +0530

    add a new field qantity in products model and show in seller dashboard

--- e_commerse/store/models.py
+++ e_commerse/store/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
     description = models.TextField()
     slug = AutoSlugField(populate_from='name', unique=True)
     photo = models.ImageField(upload_to='products/', blank=True, null=True) 
