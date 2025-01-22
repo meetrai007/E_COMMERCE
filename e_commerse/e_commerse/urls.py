4a98bbfdb248db9dcb1582f7b0be51f46347b8bb
meetrai007 <meetrai101@gmail.com>
2025-01-22 16:56:22 +0530

    add favicon on my site

--- e_commerse/e_commerse/urls.py
+++ e_commerse/e_commerse/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,6 @@ urlpatterns = [
     path('cart/',include('cart.urls')),
     path('seller/',include('seller.urls')),
     path('orders/',include('orders.urls')),
+    # Redirect favicon.ico requests to the static file
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
